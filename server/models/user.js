4f1d72520a326d184233b36ff6ea32420640c531
ChauPham90 <minhchaupham90@gmail.com>
2020-05-23 08:56:30 +0000

    added try- catch block

--- server/models/user.js
+++ server/models/user.js
@@ -4,56 +4,57 @@ const mongoose = require("mongoose");
 const crypto = require("crypto");
 
 const userSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      minlength: 5,
-      maxlength: 220,
-      trim: true,
-      required: true,
-    },
-    email: {
-      type: String,
-      unique: true,
-      required: true,
-      trim: true,
-    },
-    hashed_password: {
-      type: String,
-      required: true,
-    },
-    about: {
-      type: String,
-      trim: true,
-    },
-    salt: String,
-    role: {
-      type: Number,
-      default: 0,
-    },
+    {
+        name: {
+            type: String,
+            minlength: 5,
+            maxlength: 220,
+            trim: true,
+            required: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        hashed_password: {
+            type: String,
+            required: true,
+        },
+        about: {
+            type: String,
+            trim: true,
+        },
+        salt: String,
+        role: {
+            type: Number,
+            default: 0,
+        },
 
-    history: {
-      type: Array,
-      default: [],
+        history: {
+            type: Array,
+            default: [],
+        },
     },
-  },
-  { timestamps: true }
+    { timestamps: true }
 );
 
 userSchema.virtual("password").set(function (password) {
-  this._password = password;
-  this.salt = uuidv1();
-  this.hashed_password = this.encrypyPassword(password);
-});
+    this._password = password;
+    this.salt = uuidv1();
+    this.hashed_password = this.encrypyPassword(password);
+}).get();
 
 userSchema.methods = {
-  encrypyPassword: function (password) {
-    if (!password) return;
-    else {
-      return crypto
-        .createHmac("sha1", this.salt)
-        .update(password)
-        .digest("hex");
-    }
-  },
+    encrypyPassword: function (password) {
+        if (!password) return;
+        try {
+            return crypto
+                .createHmac("sha1", this.salt)
+                .update(password)
+                .digest("hex");
+        }
+        catch (err) { return '' }
+    },
 };
